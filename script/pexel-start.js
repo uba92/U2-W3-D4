016aeab0b63eae3de2aa99e5f43c821bdcb61d5f
Umberto Tramontano <u.tramontano92@libero.it>
2024-11-14 17:22:42 +0100

    update

--- script/pexel-start.js
+++ script/pexel-start.js
@@ -1,4 +1,4 @@
-const PEXEL_URL = 'https://api.pexels.com/v1/search?query=horse'
+const PEXEL_URL = 'https://api.pexels.com/v1/search?query=galaxy'
 
 const API_KEY = 'ayRAraaqUW3F0iycASQJRiZMaiKrsWL1Mzv1Xt4l31xsB0ipFkx1xFsp'
 
@@ -30,13 +30,25 @@ loadBtn.addEventListener('click', function () {
       for (let i = 0; i < arrImg.length; i++) {
         arrImg[i].setAttribute('src', `${data.photos[i].src.medium}`)
       }
+      let textMuted = document.querySelectorAll('small.text-muted')
+      const arrMuted = Array.from(textMuted)
+      console.log(arrMuted)
+      for (let i = 0; i < arrMuted.length; i++) {
+        arrMuted[i].innerText = `${data.photos[i].id}`
+      }
+
+      const cards = document.querySelectorAll('.card')
+      const arrOfCards = Array.from(cards)
+      for (let i = 0; i < arrOfCards.length; i++) {
+        arrOfCards[i].classList.remove('d-none')
+      }
     })
     .catch((error) => {
       console.log('error', error)
     })
 })
 
-const NEW_PEXEL_URL = 'https://api.pexels.com/v1/search?query=cow'
+const NEW_PEXEL_URL = 'https://api.pexels.com/v1/search?query=mountains'
 const loadBtnSecond = document.getElementById('load-secondary-btn')
 
 loadBtnSecond.addEventListener('click', function () {
@@ -61,6 +73,12 @@ loadBtnSecond.addEventListener('click', function () {
       for (let i = 0; i < arrImg.length; i++) {
         arrImg[i].setAttribute('src', `${data.photos[i].src.medium}`)
       }
+      let textMuted = document.querySelectorAll('small.text-muted')
+      const arrMuted = Array.from(textMuted)
+      console.log(arrMuted)
+      for (let i = 0; i < arrMuted.length; i++) {
+        arrMuted[i].innerText = `${data.photos[i].id}`
+      }
     })
     .catch((error) => {
       console.log('error', error)
@@ -75,16 +93,3 @@ for (let i = 0; i < editButton.length; i++) {
     cards[i].classList.add('d-none')
   })
 }
-
-fetch(PEXEL_URL, {
-  method: 'GET',
-  headers: {
-    Authorization: API_KEY,
-  },
-}).then((response) => {
-  if (response.ok) {
-    return response.json()
-  } else {
-    throw new Error('Errore nella richiesta!')
-  }
-})
